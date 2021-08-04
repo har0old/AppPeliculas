e44a7352de20367a67b0520516331e919528d703
har0old <garciavargas9112@gmail.com>
2021-08-04 02:34:26 -0500

    Signed-off-by: har0old <garciavargas9112@gmail.com>

---
+++ test_catalogo.py
@@ -0,0 +1,27 @@
+from Servicio.catalogo_peliculas import CatalogoPeliculas as cp
+from dominio.Pelicula import Pelicula
+
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones: ')
+        print('1. Agregar Pelicula')
+        print('2. Listar Pelicula')
+        print('3. Eliminar catalogo de peliculas')
+        print('4. Salir')
+        opcion = int(input('Escribe tu opcion (1 - 4): '))
+
+        if opcion == 1:
+            nombre_pelicula = input('Ingrese el nombre de la pelicula: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_pelicula(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+
+    except Exception as e:
+        print(f'Ocurrio un error {e}')
+        opcion = None
+else:
+    print('Salimos del programa...')
